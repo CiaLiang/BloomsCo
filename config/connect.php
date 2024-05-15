bc1c353d1ae126e2247a0b51498d5a5d9caf5234
CiaLiang <cllee8088@gmail.com>
2024-05-15 18:52:45 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -4,8 +4,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
+$dsn = 'mysql:host=bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com;port=3306;dbname=bloomsCo';
+$username = 'bloomsCo';
+$password = 'bloomsCo';
 
-$con = mysqli_connect('localhost','root','','sneakervault');
+$dbh = new PDO($dsn, $username, $password);
 if(!$con){
     die(mysqli_error($con));
 }
