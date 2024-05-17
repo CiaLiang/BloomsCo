fe799cf600679bf801169a600a8c6804914d7077
CiaLiang <cllee8088@gmail.com>
2024-05-17 19:51:05 +0800

    Add files via upload

---
+++ config/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+$con = mysqli_connect('bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com', 'blooms', 'bloomsCo123', 'bloomsCo');
+if (!$con) {
+    die(mysqli_error($con));
+}
