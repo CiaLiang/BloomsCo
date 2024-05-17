32a0a7e3572b1a2da4cd5aa83729ea4baab09e79
CiaLiang <cllee8088@gmail.com>
2024-05-17 20:22:25 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,11 +1,14 @@
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+$dsn = 'mysql:host=bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com;dbname=bloomsCo';
+$username = 'blooms';
+$password = 'bloomsCo123';
 
-$con = mysqli_connect('bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com', 'blooms', 'bloomsCo123', 'bloomsCo');
-if (!$con) {
-    die(mysqli_error($con));
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
 }
+
+?>
