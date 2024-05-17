1fd1419d785b4f09f61a493d6dba6e283dc6e183
CiaLiang <cllee8088@gmail.com>
2024-05-18 00:39:38 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,7 +1,12 @@
 <?php
-$conn = mysqli_connect('bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com', 'blooms', 'bloomsCo123', 'bloomsCo');
-define('DB_HOST', "bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com");
-define('DB_USER', "blooms");
-define('DB_PASS', "bloomsCo123");
-define('DB_NAME', "bloomsCo");
+// Establish a database connection to AWS RDS
+$host = "bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com";
+$username = "blooms";
+$password = "bloomsCo123";
+$db_name = "bloomsCo";
+
+$con = mysqli_connect($host, $username, $password, $db_name);
+if (!$con) {
+    die(mysqli_error($con));
+}
 ?>
