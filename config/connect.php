6c44c79a59c86b7a6c1c282239761cd503066c55
CiaLiang <cllee8088@gmail.com>
2024-05-17 23:42:30 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,13 +1,7 @@
 <?php
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
-$con = mysqli_connect('bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com', 'blooms', 'bloomsCo123', 'bloomsCo');
-if (!$con) {
-    die(mysqli_error($con));
-}
-
+$conn = mysqli_connect('bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com', 'blooms', 'bloomsCo123', 'bloomsCo');
+define('DB_HOST', "bloomsco.cgas1ms7nugm.us-east-1.rds.amazonaws.com");
+define('DB_USER', "blooms");
+define('DB_PASS', "bloomsCo123");
+define('DB_NAME', "bloomsCo");
 ?>
